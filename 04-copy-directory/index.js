b735d222a51d037b3ffcea8b99b920d62e96a8da
xDamneDx <xdamnedx@gmail.com>
2022-11-03 14:19:39 +0200

    feat: implement copy folder with files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require("fs");
+const path = require("path");
 
+const target = path.join(__dirname, "files");
+const destination = path.join(__dirname, "files-copy");
+
+fs.rm(destination, { recursive: true }, () => {
+  fs.mkdir(destination, { recursive: true }, () => {
+    fs.readdir(target, (_, files) => {
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(target, file),
+          path.join(destination, file),
+          () => {}
+        );
+      });
+      console.log(
+        `All files were copied...\nFrom: ${target}\nTo: ${destination}`
+      );
+    });
+  });
+});
